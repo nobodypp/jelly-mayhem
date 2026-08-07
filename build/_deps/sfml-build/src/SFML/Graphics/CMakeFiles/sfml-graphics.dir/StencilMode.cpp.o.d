e27104ee1aa28cd3014477a6b1303f25d76cc5c2
nobodypp <unconfigured@null.spigotmc.org>
2026-08-07 22:41:44 +0200

    removed build and changed cmakelists

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o: \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
- /usr/include/stdc-predef.h \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Config.hpp
--- build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/fsliwa/Code/project1/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
